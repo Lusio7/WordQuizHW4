a4e26aad2bc5ab8b2ec29c7ab9446eb5f0ab60cf
LusioF <lusiof@gmail.com>
2019-11-28 19:07:18 -0500

    creating js file and calling elements from html with getElementById

---
+++ questions.js
@@ -0,0 +1,15 @@
+const start = document.getElementById("start");
+
+const quiz = document.getElementById("quiz");
+
+const qImg = document.getElementById("questionImage");
+
+const question = document.getElementById("question");
+
+const counter = document.getElementById("counter");
+
+const timeGuage = document.getElementById("timeGuage");
+
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
