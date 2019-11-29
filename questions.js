67d02899d4f3c568e48b853f528b8b2dd68a94d0
LusioF <lusiof@gmail.com>
2019-11-28 19:18:11 -0500

    create an array with choices for the questions

--- questions.js
+++ questions.js
@@ -1,3 +1,5 @@
+/* Get elements from html file with id*/
+
 const start = document.getElementById("start");
 
 const quiz = document.getElementById("quiz");
@@ -13,3 +15,24 @@ const timeGuage = document.getElementById("timeGuage");
 const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
+
+const progress = document.getElementById("progress");
+
+const scoreContainer = document.getElementById("scoreContainer");
+
+/*  Create Questions*/
+
+let quesrions = [
+    {
+        question : "What does HTML stand for?",
+        imgSrc : "./assets/HTMLLogo.png",
+        choiceA : "Hyper Text Markup Language",
+        choiceB : "Hyperlinks Text Markup Languages",
+        choiceC : "Home Tool Markup Language",
+    },
+    {
+        
+
+    }
+],
+
