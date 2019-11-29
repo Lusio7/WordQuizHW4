2f0219cd521207ab5a67b627c0b44f7271587b6f
LusioF <lusiof@gmail.com>
2019-11-28 19:43:46 -0500

    creating the question reader

--- questions.js
+++ questions.js
@@ -29,10 +29,41 @@ let quesrions = [
         choiceA : "Hyper Text Markup Language",
         choiceB : "Hyperlinks Text Markup Languages",
         choiceC : "Home Tool Markup Language",
+        correct : "A",
     },
     {
+        question : "What does CSS stand for",
+        imgSrc : "./assets/CSSLogo.png",
+        choiceA : "Calculated Style Sheets",
+        choiceB : "Cumulative Styling System",
+        choiceC : "Cascading Style Sheets",
+
         
 
     }
 ],
+
+/* Creating the question reader*/
+
+let lastQuestionIndex = questions.length-1
+let runningQuestionIndex = 0;
+
+
+function renderQuestion (){
+    let q = questions[runningQuestionIndex];
+    qImg.innerHTML = "<img src=" + q.imgSrc + ">";
+    question.innerHTML = "<p>" + q.question + "</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+
+    runningQuestionIndex = 0;
+    renderQuestion()
+
+    runningQuestionIndex++
+    renderQuestion()
+}
+
+
+
 
